2791e39c45648c2afbae7fb11fce28b98635676e
Sergey Hamzatov <drkazak@gmail.com>
2019-03-01 11:54:33 +0300

    Added initial description and gitignore

--- BullAndCows/BullAndCows/BullAndCows.cpp
+++ BullAndCows/BullAndCows/BullAndCows.cpp
@@ -1,10 +1,13 @@
-// BullAndCows.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 
+//using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::cout << "--- Welcome to the best game ever!" << std::endl
+		<< "--- Designed by Jexser" << std::endl
+		<< "--- Game description here" << std::endl;
+
+	return 0;
 }
