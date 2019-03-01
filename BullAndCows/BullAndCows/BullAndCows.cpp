b8250f895f2dd40729c75adbf8492872ca6d235c
Sergey Hamzatov <drkazak@gmail.com>
2019-03-01 11:56:47 +0300

    Updated description

--- BullAndCows/BullAndCows/BullAndCows.cpp
+++ BullAndCows/BullAndCows/BullAndCows.cpp
@@ -5,9 +5,13 @@
 
 int main()
 {
+	constexpr int WORD_LENGTH = 5;
+
 	std::cout << "--- Welcome to the best game ever!" << std::endl
 		<< "--- Designed by Jexser" << std::endl
-		<< "--- Game description here" << std::endl;
+		<< "--- Game description here" << std::endl
+		<< std::endl
+		<< "Can you guess the " << WORD_LENGTH << " letters isogram that I guessed?" << std::endl;
 
 	return 0;
 }
